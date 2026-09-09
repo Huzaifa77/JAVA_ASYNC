70b44bf9b5a48f6b30bec70c14836aacd96739aa
Huzaifa77 <mohammedzaif77@gmail.com>
2026-09-10 00:24:18 +0530

    updated

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,19 +5,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        try (ExecutorService executor = Executors.newFixedThreadPool(3)) {
 
 //        Runnable tsk = () -> System.out.println("Executed task");
-        //submit method takes either a Runnable or callable. This above runnable is an FunctionalInterface so we can either create an implementation for this Runnable interface or since its a FunctionalInterface we can directly write the lambda function inside the below submit method.
+            //submit method takes either a Runnable or callable. This above runnable is an FunctionalInterface so we can either create an implementation for this Runnable interface or since its a FunctionalInterface we can directly write the lambda function inside the below submit method.
 //        executor.submit(tsk);
-        executor.submit(() -> System.out.println("Executed task on "+ Thread.currentThread().getName()));
+            executor.submit(() -> System.out.println("Executed task on " + Thread.currentThread().getName()));
+        }
 
         System.out.println("Main execution on "+ Thread.currentThread().getName());
     }
