553d125c53a3cbd65916950663b1139f50b81326
Huzaifa77 <mohammedzaif77@gmail.com>
2026-09-12 00:29:30 +0530

    changes for submit with notes

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
-    void contextLoads() throws InterruptedException {
+    void contextLoads() throws InterruptedException, ExecutionException {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
 //        Runnable tsk = () -> System.out.println("Executed task");
@@ -24,42 +26,69 @@ class DemoApplicationTests {
 //
 //        System.out.println("Main execution on "+ Thread.currentThread().getName());
 
-        executorService.execute(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Email sent");
-        });
+//        executorService.execute(() -> {
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            System.out.println("Email sent");
+//        });
+//
+//        executorService.execute(() -> {
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            System.out.println("SMS sent");
+//        });
+//
+//        executorService.execute(() -> {
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            System.out.println("Whatsapp sent");
+//        });
+//
+//        System.out.println("ALL notifications sent");
 
-        executorService.execute(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("SMS sent");
-        });
 
-        executorService.execute(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Whatsapp sent");
-        });
-
-        System.out.println("ALL notifications sent");
-
-        //        executorService.submit(tsk); // this accept either Runnable or callable
-
-    //the problem with executor is it a fire and forger. And also since it take runnable and runnable returns only void so if there is any operation that might return something we cant use that and we cant know if the operation is done. Also if any errors and exceptions occurs the thread execution will be stopeed.
+    //the problem with executor is it a fire and forger. And also since it take runnable and runnable returns only void so if there is any operation that might return something we cant use that and we cant know if the operation is done also if any errors and exceptions occurs the thread execution will be stopped.
         //so better use this for any background job or logging etc
-        executorService.shutdown();
 
-        executorService.awaitTermination(5, java.util.concurrent.TimeUnit.SECONDS);
+        //so for resolving the issue of not knowing when the thread is executed and also to may be get the value of the execution we should use the method submit.
+        //        executorService.submit(tsk); // this accept either Runnable or callable
+        //and submit return the type of future.
+        //future's get() method stops the main thread execution and wait till that future's thread is completed . and also get() method can also return a value which we can use
+        //also for submit if any error or exception happens it boil to the main method that calls but the thread doesn't stop execution
+
+        Future<Integer> exec1 = executorService.submit(() -> {
+            Thread.sleep(2000);
+            System.out.println("FD balance");
+            return 300;
+        });
+
+        Future<Integer> exec2 = executorService.submit(() -> {
+            Thread.sleep(2000);
+            System.out.println("Current balance");
+            return 3400;
+        });
+
+        Future<Integer> exec3 = executorService.submit(() -> {
+            Thread.sleep(2000);
+            System.out.println("Savings balance");
+            return 7000;
+        });
+
+        System.out.println("Total: "+ (exec1.get() + exec2.get() + exec3.get()));
+
+
+        executorService.shutdown(); // this is also needed to tell JVM that no need for threads to wait for any other methods
+
+//        executorService.awaitTermination(5, java.util.concurrent.TimeUnit.SECONDS);
 
     }
 
